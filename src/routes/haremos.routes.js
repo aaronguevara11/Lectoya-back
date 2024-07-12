f9ac7e8a461ab4e525befa425e550cbffd96f369
aaronguevara11 <guevaraaaroncarrasco@gmail.com>
2024-07-11 20:24:25 -0500

    Actualizado

--- src/routes/haremos.routes.js
+++ src/routes/haremos.routes.js
@@ -154,4 +154,40 @@ router.post("/agregarRespuesta", async (req, res) => {
   }
 });
 
+router.post("/enviarRespuesta", async (req, res) => {
+  try {
+    const token = req.header("Authorization");
+    jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+      if (err) {
+        res.json({
+          message: "Error en el token",
+        });
+      } else {
+        const { pregunta, respuesta, id } = req.body;
+        const idAlumno = payload.id;
+        const nombre = payload.nombre;
+        const apellido = payload.apaterno;
+
+        await prisma.res_haremos.create({
+          data: {
+            idHaremos: Number(id),
+            pregunta: pregunta,
+            respuesta: respuesta,
+            idAlumno: Number(idAlumno),
+            nombre: nombre,
+            apaterno: apellido,
+          },
+        });
+        res.json({
+          message: "Respuesta enviada",
+        });
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error en el servidor",
+    });
+  }
+});
+
 export default router;
